53376c9b287b83eb3cc9d06ccbaf4a4a59a3ecdd
mo1262008tito-oss <mo1262008tito@gmail.com>
2026-01-04 18:17:15 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // هذا السطر هو الحل لمشكلة الشاشة البيضاء وخطأ الـ MIME type
+  base: './', // يضمن أن جميع روابط الملفات تبدأ بمسار نسبي
+  build: {
+    outDir: 'dist', // المجلد الذي سيقرأ منه Netlify
+  }
 })
