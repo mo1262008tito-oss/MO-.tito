c185d2a8ade6f1b4d604b56615ff19a9a012dd4a
mo1262008tito-oss <mo1262008tito@gmail.com>
2026-01-05 07:36:04 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ import Religious from './pages/Religious';
 import About from './pages/About';
 import Library from './pages/Library';
 
-import './Global.css';
+
 
 const ProtectedRoute = ({ children, isActive, loading, redirectPath = "/highschool" }) => {
   if (loading) return null; 
@@ -118,6 +118,7 @@ function App() {
 
 
 export default App;
+
 
 
 
