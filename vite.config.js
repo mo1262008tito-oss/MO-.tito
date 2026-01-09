ca382970901f21ff5daca5a0e59d3a6792e9b40f
mo1262008tito-oss <mo1262008tito@gmail.com>
2026-01-09 16:20:55 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // يضمن أن جميع روابط الملفات تبدأ بمسار نسبي
+  base: './', 
   build: {
-    outDir: 'dist', // المجلد الذي سيقرأ منه Netlify
+    outDir: 'dist',
+    // 1. زيادة حد التحذير لحجم الملفات ليناسب الكود الضخم الجديد
+    chunkSizeWarningLimit: 2000, 
+    
+    // 2. تحسين عملية تقسيم الكود (Code Splitting) لضمان سرعة التحميل
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // وضع المكتبات الخارجية في ملف منفصل
+          }
+        }
+      }
+    }
   }
 })
