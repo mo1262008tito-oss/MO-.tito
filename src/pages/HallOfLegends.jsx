659ab33bafe61c92f89e0aacf90da7a4f3549335
mo1262008tito-oss <mo1262008tito@gmail.com>
2026-02-18 12:40:30 +0200

    Update HallOfLegends.jsx

--- src/pages/HallOfLegends.jsx
+++ src/pages/HallOfLegends.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo, useRef } from 'react';
+import React, { useState, useEffect, useMemo, useRef, useCallback } from 'react';
 import { motion, AnimatePresence, useScroll, useSpring } from 'framer-motion';
 import { db, auth } from '../firebase'; // تأكد من مسار ملف الفايربيس
 
